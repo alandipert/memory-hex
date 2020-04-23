f6bc66cd2cd6c6ba91791138583847b29a88f541
Alan Dipert <alan@dipert.org>
2020-04-23 22:58:04 +0000

    Puzzling failing test involving a reactiveValues update

--- tests/testthat/test-hex.R
+++ tests/testthat/test-hex.R
@@ -7,15 +7,29 @@ test_that("hex module behaves correctly", {
   block <- reactiveValues(x = NULL)
 
   testServer(hex, {
+
     expect_equal(hex_logo, !!shiny_logo)
 
     # Initially, hexes are neither found nor visible.
     expect_false(click_status$found)
     expect_false(click_status$show)
 
-    # If not already found, upon being clicked, the hex becomes visible.
-    session$click("hex_click")
+    # On the first click, the hex becomes visible.
+    session$setInputs(hex_click = 0)
     expect_true(click_status$show)
+
+    # Clicked again, the hex is hidden.
+    session$setInputs(hex_click = input$hex+1)
+    expect_false(click_status$show)
+
+    # Click again to make visible
+    session$setInputs(hex_click = input$hex+1)
+    expect_true(click_status$show)
+
+    # Simulate another module being clicked while this one is still visible.
+    block$x <- hex_logo
+    expect_true(click_status$found)
+
   },
     hex_logo = shiny_logo,
     reset = reset,
